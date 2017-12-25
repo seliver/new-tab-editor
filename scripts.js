07486b7d0e4a3ab93265342ceb74d71b1574ac8b
Alexey Seliverstov <alexeyseliverstov.dev@gmail.com>
2017-12-25 23:52:50 +0000

    add string length

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 window.onload = function() {
     var d = document.getElementById("editor");
+    var info = document.getElementById('info');
     var editor = CodeMirror.fromTextArea(d, {
         lineNumbers: true
     });
+    var setLength = function(){
+        info.innerHTML = 'Length: '+editor.getValue().length;
+    };
+    setLength();
+    editor.on("keyup", setLength);
 };
 
